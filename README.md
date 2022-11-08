d392c3f7ea372ad8bd187ae62ba2e1cad8628756
aniaan <fjn.aniaan@gmail.com>
2022-11-08 18:06:11 +0800

    fix(prefix): prefix

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # eg-router-benchmark
+
+## static
+
+vm1 static 2.6w/tps
+http://vm-1:10080/a35fe7f7fe8217b4369a0af4244d1fca
+
+./hey -n 900 -c 50 -m GET http://vm-1:10080/a35fe7f7fe8217b4369a0af4244d1fca
+> 24068.3219
+
+./hey -n 90000 -c 100 -m GET http://vm-1:10080/a35fe7f7fe8217b4369a0af4244d1fca
+> 26077.3742
+
+./hey -n 90000 -c 120 -m GET http://vm-1:10080/a35fe7f7fe8217b4369a0af4244d1fca
+> 27219.1946
+
+vm2 static 2.7w/tps
+
+http://vm-2:10080/a35fe7f7fe8217b4369a0af4244d1fca
+
+./hey -n 900 -c 50 -m GET http://vm-2:10080/a35fe7f7fe8217b4369a0af4244d1fca
+> 25078.4517
+
+./hey -n 90000 -c 100 -m GET http://vm-2:10080/a35fe7f7fe8217b4369a0af4244d1fca
+> 27391.6867
+
+./hey -n 90000 -c 120 -m GET http://vm-2:10080/a35fe7f7fe8217b4369a0af4244d1fca
+> 27401.6701
+
+vm3 static 6.5w/tps
+
+http://vm-3:10080/a35fe7f7fe8217b4369a0af4244d1fca
+
+./hey -n 900 -c 50 -m GET http://vm-3:10080/a35fe7f7fe8217b4369a0af4244d1fca
+> 41687.5741
+
+./hey -n 90000 -c 100 -m GET http://vm-3:10080/a35fe7f7fe8217b4369a0af4244d1fca
+> 65122.1171
+
+./hey -n 90000 -c 120 -m GET http://vm-3:10080/a35fe7f7fe8217b4369a0af4244d1fca
+> 65638.4786
---
+++ gen_prefix.py
@@ -0,0 +1,53 @@
+import hashlib
+import json
+from ruamel.yaml import YAML
+
+
+def gen_ordered_router_rules(count: int = 10000):
+
+    config = {
+        "kind": "HTTPServer",
+        "name": "server-demo",
+        "port": 10080,
+        "keepAlive": True,
+        "https": False,
+    }
+
+    paths = []
+    for i in range(1, count + 1):
+        path = "/" + hashlib.md5(f"{i}".encode("utf-8")).hexdigest()
+        paths.append({"pathPrefix": path, "backend": "mock", "methods": ["GET"]})
+
+    config["rules"] = [{"paths": paths}]
+
+    with open("./prefix/order.yaml", "w") as out:
+        yaml = YAML()
+        yaml.dump(config, out)
+
+
+def gen_radix_tree_router_rules(count: int = 10000):
+    config = {
+        "kind": "HTTPServer",
+        "name": "server-demo",
+        "port": 10080,
+        "keepAlive": True,
+        "https": False,
+        "routerKind": "RadixTree",
+    }
+
+    paths = []
+    for i in range(1, count + 1):
+        path = "/" + hashlib.md5(f"{i}".encode("utf-8")).hexdigest() + "/*"
+        paths.append({"path": path, "backend": "mock", "methods": ["GET"]})
+
+    config["rules"] = [{"paths": paths}]
+
+    with open("./prefix/radixtree.yaml", "w") as out:
+        yaml = YAML()
+        yaml.dump(config, out)
+
+
+# 5000: a35fe7f7fe8217b4369a0af4244d1fca
+if __name__ == "__main__":
+    gen_ordered_router_rules()
+    gen_radix_tree_router_rules()
